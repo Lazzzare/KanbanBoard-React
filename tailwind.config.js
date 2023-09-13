727e1b8b9cdadffd51eeb9f8956573aa14f422eb
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-13 15:11:47 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBackgroundColor: "#0D1117",
+        columnBackgroundColor: "#161c22",
+      },
+    },
   },
   plugins: [],
-}
+};
