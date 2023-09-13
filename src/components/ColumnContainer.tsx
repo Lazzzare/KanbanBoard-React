7ec1cdc6254e083ef1947b608e8e8a4d13ea46af
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-13 15:44:23 +0400

    Update ColumnContainer.tsx

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,5 +1,35 @@
-const ColumnContainer = () => {
-  return <div>ColumnContainer</div>;
+import DeleteIcons from "../icons/DeleteIcons";
+import { Column, Id } from "../types";
+
+interface Props {
+  column: Column;
+  deleteColumn: (id: Id) => void;
+}
+
+const ColumnContainer = ({ column, deleteColumn }: Props) => {
+  return (
+    <div className="bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col">
+      <div
+        className="bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded-md rounded-b-none
+      p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between"
+      >
+        <div className="flex gap-2 items-center">
+          <div className="flex justify-center items-center bg-columnBackgroundColor px-2 py-1 text-sm rounded-full">
+            0
+          </div>
+          {column.title}
+        </div>
+        <button
+          onClick={() => deleteColumn(column.id)}
+          className="stroke-gray-500 hover:stroke-white hover:bg-columnBackgroundColor rounded px-1 py-2"
+        >
+          <DeleteIcons />
+        </button>
+      </div>
+      <div className="flex flex-grow">Content</div>
+      <div>Footer</div>
+    </div>
+  );
 };
 
 export default ColumnContainer;
