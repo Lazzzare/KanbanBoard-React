97b0f721655c917d240e07f3dcfc16c59a210eae
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-26 17:52:20 +0400

    Update ColumnContainer.tsx

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,14 +1,27 @@
 import DeleteIcons from "../icons/DeleteIcons";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { Column, Id } from "../types";
+import { Column, Id, Task } from "../types";
+import { useState } from "react";
+import PlusIcon from "../icons/PlusIcon";
+import TaskCard from "./TaskCard";
 
 interface Props {
   column: Column;
   deleteColumn: (id: Id) => void;
+  updateColumn: (id: Id, title: string) => void;
+  createTask: (ColumnId: Id) => void;
+  tasks: Task[];
 }
 
-const ColumnContainer = ({ column, deleteColumn }: Props) => {
+const ColumnContainer = ({
+  column,
+  deleteColumn,
+  updateColumn,
+  createTask,
+  tasks,
+}: Props) => {
+  const [editMode, setEditMode] = useState(false);
   const {
     setNodeRef,
     attributes,
@@ -22,6 +35,7 @@ const ColumnContainer = ({ column, deleteColumn }: Props) => {
       type: "Column",
       column,
     },
+    disabled: editMode,
   });
 
   const style = {
@@ -48,6 +62,7 @@ const ColumnContainer = ({ column, deleteColumn }: Props) => {
       <div
         {...attributes}
         {...listeners}
+        onClick={() => setEditMode(true)}
         className="bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded-md rounded-b-none
       p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between"
       >
@@ -55,7 +70,22 @@ const ColumnContainer = ({ column, deleteColumn }: Props) => {
           <div className="flex justify-center items-center bg-columnBackgroundColor px-2 py-1 text-sm rounded-full">
             0
           </div>
-          {column.title}
+          {!editMode ? column.title : null}
+          {editMode ? (
+            <input
+              className="bg-black focus:border-rose-500 border rounded outline-none px-2"
+              value={column.title}
+              onChange={(e) => {
+                updateColumn(column.id, e.target.value);
+              }}
+              autoFocus
+              onBlur={() => setEditMode(false)}
+              onKeyDown={(e) => {
+                if (e.key !== "Enter") return;
+                setEditMode(false);
+              }}
+            />
+          ) : null}
         </div>
         <button
           onClick={() => deleteColumn(column.id)}
@@ -64,8 +94,20 @@ const ColumnContainer = ({ column, deleteColumn }: Props) => {
           <DeleteIcons />
         </button>
       </div>
-      <div className="flex flex-grow">Content</div>
-      <div>Footer</div>
+      <div className="flex flex-grow flex-col gap-4 p-2 overflow-x-hidden overflow-y-auto">
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </div>
+      {/* ContentFooter */}
+      <button
+        onClick={() => createTask(column.id)}
+        className="flex gap-2 items-center border-columnBackgroundColor border-2 rounded-md p-4
+       border-x-columnBackgroundColor hover:bg-mainBackgroundColor hover:text-rose-500 active:bg-black"
+      >
+        <PlusIcon />
+        Add task
+      </button>
     </div>
   );
 };
