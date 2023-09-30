c5cf90b05dba2d312393bdde1c12a2cfc197c4e2
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-09-30 14:18:35 +0400

    Update TaskCard.tsx

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,17 +1,37 @@
-import { Task } from "../types";
+import { Id, Task } from "../types";
+import DeleteIcons from "../icons/DeleteIcon";
+import { useState } from "react";
 
 interface Props {
   task: Task;
+  deleteTask: (id: Id) => void;
 }
 
-const TaskCard = ({ task }: Props) => {
+const TaskCard = ({ task, deleteTask }: Props) => {
+  const [mouseOver, setMouseOver] = useState(false);
+
   return (
     <div
-      className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items center flex text-left rounded-xl hover:ring-2 
-      hover:ring-inset hover:ring-rose-500 cursor-grab"
+      className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 
+      hover:ring-inset hover:ring-rose-500 cursor-grab relative"
       key={task.id}
+      onMouseEnter={() => {
+        setMouseOver(true);
+      }}
+      onMouseLeave={() => {
+        setMouseOver(false);
+      }}
     >
       {task.content}
+      {mouseOver ? (
+        <button
+          onClick={() => deleteTask(task.id)}
+          className="stroke-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor p-2 rounded-md
+        opacity-60 hover:opacity-100"
+        >
+          <DeleteIcons />
+        </button>
+      ) : null}
     </div>
   );
 };
