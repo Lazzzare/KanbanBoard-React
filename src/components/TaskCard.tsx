f5418372d252ea11ed6a70e7273c1561a4ec53eb
Lazare Chkhartishvili <96971788+Lazzzare@users.noreply.github.com>
2023-10-02 17:05:39 +0400

    Update TaskCard.tsx

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,39 +1,118 @@
-import { Id, Task } from "../types";
-import DeleteIcons from "../icons/DeleteIcon";
 import { useState } from "react";
+import DeleteIcon from "../icons/DeleteIcon";
+import { Id, Task } from "../types";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface Props {
   task: Task;
   deleteTask: (id: Id) => void;
+  updateTask: (id: Id, content: string) => void;
 }
 
-const TaskCard = ({ task, deleteTask }: Props) => {
-  const [mouseOver, setMouseOver] = useState(false);
+function TaskCard({ task, deleteTask, updateTask }: Props) {
+  const [mouseIsOver, setMouseIsOver] = useState(false);
+  const [editMode, setEditMode] = useState(true);
+
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: task.id,
+    data: {
+      type: "Task",
+      task,
+    },
+    disabled: editMode,
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  const toggleEditMode = () => {
+    setEditMode((prev) => !prev);
+    setMouseIsOver(false);
+  };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="
+        opacity-30
+      bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl border-2 border-rose-500  cursor-grab relative
+      "
+      />
+    );
+  }
+
+  if (editMode) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        {...attributes}
+        {...listeners}
+        className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative"
+      >
+        <textarea
+          className="
+        h-[90%]
+        w-full resize-none border-none rounded bg-transparent text-white focus:outline-none
+        "
+          value={task.content}
+          autoFocus
+          placeholder="Task content here"
+          onBlur={toggleEditMode}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && e.shiftKey) {
+              toggleEditMode();
+            }
+          }}
+          onChange={(e) => updateTask(task.id, e.target.value)}
+        />
+      </div>
+    );
+  }
 
   return (
     <div
-      className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 
-      hover:ring-inset hover:ring-rose-500 cursor-grab relative"
-      key={task.id}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      onClick={toggleEditMode}
+      className="bg-mainBackgroundColor p-2.5 h-[100px] min-h-[100px] items-center flex text-left rounded-xl hover:ring-2 hover:ring-inset hover:ring-rose-500 cursor-grab relative task"
       onMouseEnter={() => {
-        setMouseOver(true);
+        setMouseIsOver(true);
       }}
       onMouseLeave={() => {
-        setMouseOver(false);
+        setMouseIsOver(false);
       }}
     >
-      {task.content}
-      {mouseOver ? (
+      <p className="my-auto h-[90%] w-full overflow-y-auto overflow-x-hidden whitespace-pre-wrap">
+        {task.content}
+      </p>
+
+      {mouseIsOver && (
         <button
-          onClick={() => deleteTask(task.id)}
-          className="stroke-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor p-2 rounded-md
-        opacity-60 hover:opacity-100"
+          onClick={() => {
+            deleteTask(task.id);
+          }}
+          className="stroke-white absolute right-4 top-1/2 -translate-y-1/2 bg-columnBackgroundColor p-2 rounded opacity-60 hover:opacity-100"
         >
-          <DeleteIcons />
+          <DeleteIcon />
         </button>
-      ) : null}
+      )}
     </div>
   );
-};
+}
 
 export default TaskCard;
